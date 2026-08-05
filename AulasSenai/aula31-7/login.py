15094b8f78eefa9432d3f6b063ccdb4761eff0c5
JoaoMaldaner <joao.pcmaldaner@outlook.com>
2026-08-05 20:43:56 -0300

    correção da atividade do dia 31

--- AulasSenai/aula31-7/login.py
+++ AulasSenai/aula31-7/login.py
@@ -1,21 +1,21 @@
 import tkinter as tk
 root = tk.Tk()
 root.title("Frames aula 31-7")
-root.geometry("600x600")
+root.geometry("320x480")
 #root.config(bg="#0C2FA1")
 
-tk.Label(root, text="Faça seu login").pack(ipady=50)  # fill="x" faz com que o widget ocupe toda a largura disponível, e ipady adiciona um preenchimento interno vertical para aumentar a altura do widget.
+tk.Label(root, text="Faça seu login", font="arial 32 bold").pack(ipady=5, fill="x")  # fill="x" faz com que o widget ocupe toda a largura disponível, e ipady adiciona um preenchimento interno vertical para aumentar a altura do widget.
 
-userImage = tk.PhotoImage(file="images.png")  # Certifique-se de que o caminho para a imagem esteja correto
+userImage = tk.PhotoImage(file="images.png").subsample(1, 1)  # subsample(2, 2) reduz a imagem para metade do tamanho original em ambas as dimensões (largura e altura).
 
-label = tk.Label(root, image=userImage)
+label = tk.Label(root, image=userImage, relief=tk.SUNKEN, bd=2)  # relief=tk.SUNKEN cria um efeito de relevo afundado ao redor do widget, e bd=2 define a largura da borda em 2 pixels.
 label.pack(expand=True)
 
-tk.Label(root, text="Usuário").pack(anchor=tk.W) #ancor=tk.W significa que o widget será ancorado à esquerda (West) do seu contêiner pai. Isso faz com que o widget seja posicionado à esquerda, independentemente do tamanho do contêiner.
+tk.Label(root, text="Usuário").pack(anchor=tk.W, padx=20) #ancor=tk.W significa que o widget será ancorado à esquerda (West) do seu contêiner pai. Isso faz com que o widget seja posicionado à esquerda, independentemente do tamanho do contêiner.
 user = tk.Entry(root)
 user.pack()
 
-tk.Label(root, text="Senha").pack(anchor=tk.W)
+tk.Label(root, text="Senha").pack(anchor=tk.W, padx=20)
 password = tk.Entry(root, show="*")
 password.pack()
 
@@ -26,10 +26,9 @@ botao.pack(pady=10)
 
 
 
-tk.Checkbutton(root, text="Lembrar-me").pack(side=tk.LEFT)
-tk.Label(root, text="Esqueceu sua senha?").pack(side=tk.RIGHT)
+tk.Checkbutton(root, text="Lembrar-me").pack(side=tk.LEFT, padx=20, pady=5)
+tk.Label(root, text="Esqueceu sua senha?", fg="blue", cursor="hand2").pack(side=tk.RIGHT, padx=20, pady=5)
 
 
 root.mainloop()
-
 
